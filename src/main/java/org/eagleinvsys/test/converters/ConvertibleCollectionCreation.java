c703fd67d733d208b4cc31c6851f6d2ef8b14572
J-Bakuli <julia.bakuli@yandex.ru>
2022-03-23 21:25:39 +0300

    Add ConvertibleCollectionCreation

--- src/main/java/org/eagleinvsys/test/converters/ConvertibleCollectionCreation.java
+++ src/main/java/org/eagleinvsys/test/converters/ConvertibleCollectionCreation.java
@@ -1,10 +1,8 @@
 package org.eagleinvsys.test.converters;
 
-import org.eagleinvsys.test.converters.impl.CsvCollection;
-
 import java.util.List;
 import java.util.Map;
 
 public interface ConvertibleCollectionCreation {
-    CsvCollection create(List<Map<String, String>> collectionToConvert);
+    ConvertibleCollection create(List<Map<String, String>> collectionToConvert);
 }
