52d82a32f86f2cc310330ad518fb4c5fc372c5d4
J-Bakuli <julia.bakuli@yandex.ru>
2022-03-25 14:31:59 +0300

    Changed naming in CsvConvertibleCollectionCreation

--- src/main/java/org/eagleinvsys/test/converters/impl/CsvConvertibleCollectionCreation.java
+++ src/main/java/org/eagleinvsys/test/converters/impl/CsvConvertibleCollectionCreation.java
@@ -1,7 +1,8 @@
 package org.eagleinvsys.test.converters.impl;
 
+import org.eagleinvsys.test.converters.ConvertibleCollection;
 import org.eagleinvsys.test.converters.ConvertibleCollectionCreation;
-import org.eagleinvsys.test.converters.Validation.Validation;
+import org.eagleinvsys.test.converters.validation.Validation;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,14 +10,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class CsvDataItemCreation implements ConvertibleCollectionCreation {
+public class CsvConvertibleCollectionCreation implements ConvertibleCollectionCreation {
     @Override
-    public CsvCollection create(List<Map<String, String>> collectionToConvert) {
-        if (Validation.checkIfCollectionToConvertIsEmpty(collectionToConvert)) {
+    public ConvertibleCollection create(List<Map<String, String>> collectionToConvert) {
+        if (Validation.checkIfStandardCollectionToConvertIsEmpty(collectionToConvert)) {
             return new CsvCollection(Collections.emptyList());
         }
         return new CsvCollection(collectionToConvert.stream()
-                .map(dataItem -> new CsvDataItem(new HashMap<>(dataItem)))
+                .map(record -> new CsvDataItem(new HashMap<>(record)))
                 .collect(Collectors.toList())
         );
     }
